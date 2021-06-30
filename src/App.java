dcf91e1c180078e9811127c5cd6de5fc8783df3e
jguzmanp695 <jguzman0695@gmail.com>
2021-06-29 21:03:16 -0500

    Second class java

--- src/App.java
+++ src/App.java
@@ -1,8 +1,13 @@
 public class App {
 
 
-    
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        int resultado = 15 + 32;
+        String res = "Hola";
+        double resultado2 = 25 / 3;
+        float resultado3 = 58 * 21;
     }
 }
